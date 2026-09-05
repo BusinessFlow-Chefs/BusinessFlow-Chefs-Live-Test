1a35d46dc4b5fd9747bd3889222ed19250e634a7
BusinessFlow-Chefs <darrenhjrichardson2@gmail.com>
2026-09-05 15:00:41 +0200

    Add offline shell service worker foundation

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='businessflow-shell-v1';
+const SHELL=['./','./index.html','./bf-core.js','./bf-platform.js','./service-sequence.html','./chef-mode.html','./style.css','./assets/home-master.webp'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});
