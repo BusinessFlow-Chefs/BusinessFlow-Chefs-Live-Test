0d2d5569864a8f645ea57a7b706b4df748315c85
BusinessFlow-Chefs <darrenhjrichardson2@gmail.com>
2026-09-05 15:22:20 +0200

    Cache Charter and Guest screens for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='businessflow-shell-v1';
-const SHELL=['./','./index.html','./bf-core.js','./bf-platform.js','./service-sequence.html','./chef-mode.html','./style.css','./assets/home-master.webp'];
+const CACHE='businessflow-shell-v2';
+const SHELL=['./','./index.html','./bf-core.js','./bf-platform.js','./charter.html','./guests.html','./service-sequence.html','./chef-mode.html','./style.css','./assets/home-master.webp'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
