0365fb9b5a3716a3dc2f0b68a06540e6ae0b4a51
BusinessFlow-Chefs <darrenhjrichardson2@gmail.com>
2026-09-05 20:24:38 +0200

    Drop broken unified stylesheet from offline shell cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='businessflow-shell-v8';
-const SHELL=['./','./index.html','./bf-core.js','./bf-platform.js','./bf-theme.css','./bf-unified.css','./bf-menu.js','./bf-menu-themes.js','./bf-menu-structure.js','./bf-recipes.js','./bf-recipes-courses.js','./bf-stock.js','./bf-sourcing.js','./charter.html','./guests.html','./menu-builder.html','./dish-details.html','./inventory.html','./provisioning.html','./journey.html','./sourcing.html','./suppliers.html','./service-sequence.html','./chef-mode.html','./style.css','./assets/home-master.webp'];
+const CACHE='businessflow-shell-v9';
+const SHELL=['./','./index.html','./bf-core.js','./bf-platform.js','./bf-theme.css','./bf-menu.js','./bf-menu-themes.js','./bf-menu-structure.js','./bf-recipes.js','./bf-recipes-courses.js','./bf-stock.js','./bf-sourcing.js','./charter.html','./guests.html','./menu-builder.html','./dish-details.html','./inventory.html','./provisioning.html','./journey.html','./sourcing.html','./suppliers.html','./service-sequence.html','./chef-mode.html','./style.css','./assets/home-master.webp'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
